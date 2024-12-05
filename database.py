30b187bf5bd9c8fa28ea784171e11ca42f91916d
jfoeks <rfta002@mail.ru>
2024-12-05 11:06:30 +0200

    .

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Создаем подключение к базе данных SQLite
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-# Сессия для взаимодействия с базой данных
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
